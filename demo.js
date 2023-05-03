10e54542eceb284d01b877138f27872917b573c5
5Knight2 <pratiktarale258@gmail.com>
2023-05-03 13:32:06 +0530

    DOM-store objects

--- demo.js
+++ demo.js
@@ -5,8 +5,14 @@ form.addEventListener('submit',add);
 function add(e){
     e.preventDefault();
     let name=document.querySelector('#name');
-    localStorage.setItem('name'+localStorage.length,name.value);
     let email=document.querySelector('#email');
-    localStorage.setItem('email'+localStorage.length,email.value);
+
+    let myobj={
+        name:name.value,
+        email:email.value
+    };
+    localStorage.setItem('my obj',JSON.stringify(myobj));
+    
+    
     
 }
