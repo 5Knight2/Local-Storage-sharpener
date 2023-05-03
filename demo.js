7c1ba69c177dbc4ed2e7c1e867aac68d2d29743a
5Knight2 <pratiktarale258@gmail.com>
2023-05-03 22:31:11 +0530

    DOM-delete data from local storage

--- demo.js
+++ demo.js
@@ -1,6 +1,33 @@
 let form=document.querySelector('#my-form');
+let ul=document.querySelector('ul');
 
 form.addEventListener('submit',add);
+ul.addEventListener('click',remove);
+
+
+function remove(e){
+    e.preventDefault();
+    if(e.target.classList.contains('delete')){
+        console.log(e.target.parentElement);
+        
+
+        for(let i=0;i<localStorage.length;i++){
+            let obj=JSON.parse(localStorage.getItem(localStorage.key(i)));
+            let str=obj.name+'-'+obj.email+'-'+obj.mobile;
+            
+            let str2=e.target.parentElement.textContent;
+            str2=str2.substring(0,str2.length-6);
+           
+            if(str==str2){
+                localStorage.removeItem(obj.email);
+                
+            }
+        }
+
+        ul.removeChild(e.target.parentElement);
+        
+    }
+}
 
 function add(e){
     e.preventDefault();
@@ -18,9 +45,12 @@ function add(e){
     let ul=document.querySelector('ul');
     let li=document.createElement('li');
     li.appendChild(document.createTextNode(name.value+'-'+email.value+'-'+mob.value));
-    ul.appendChild(li);
+    let btn=document.createElement('button');
+    btn.style.backgroundColor='red';
+    btn.appendChild(document.createTextNode('Delete'));
+    btn.className='delete';
+
     
-    
-    
-    
+    li.appendChild(btn);
+    ul.appendChild(li);    
 }
