c4dc789c7786d8862f4e1bbc3840c4b83d6f5f84
5Knight2 <pratiktarale258@gmail.com>
2023-05-03 13:55:03 +0530

    DOM-store multiple objects

--- demo.js
+++ demo.js
@@ -6,12 +6,20 @@ function add(e){
     e.preventDefault();
     let name=document.querySelector('#name');
     let email=document.querySelector('#email');
+    let mob=document.querySelector('#mob');
 
     let myobj={
         name:name.value,
-        email:email.value
+        email:email.value,
+        mobile:mob.value
     };
-    localStorage.setItem('my obj',JSON.stringify(myobj));
+    localStorage.setItem(email.value,JSON.stringify(myobj));
+
+    let ul=document.querySelector('ul');
+    let li=document.createElement('li');
+    li.appendChild(document.createTextNode(name.value+'-'+email.value+'-'+mob.value));
+    ul.appendChild(li);
+    
     
     
     
